712b06b86cb99b2019629898a7483b36900fd54b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-07-15 01:03:06 +0700

    Implements putHistory and renderHistory function

--- assets/calculator.js
+++ assets/calculator.js
@@ -59,14 +59,16 @@ function performCalculation() {
         result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
     }
 
-    calculator.displayNumber = result;
-
     const history = {
         firstNumber: calculator.firstNumber,
         secondNumber: calculator.displayNumber,
         operator: calculator.operator,
         result: result
     }
+
+    putHistory(history);
+    calculator.displayNumber = result;
+    renderHistory();
 }
 
 const buttons = document.querySelectorAll(".button");

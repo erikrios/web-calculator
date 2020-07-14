ba98da09b23cdde5510c6a28c7765132786912ca
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-07-15 00:46:49 +0700

    Create a function to put history

--- assets/storage.js
+++ assets/storage.js
@@ -1,5 +1,24 @@
 const CACHE_KEY = "calculation_history";
 
 function checkForStorage() {
-    return typeof(Storage) !== "undefined";
+    return typeof (Storage) !== "undefined";
+}
+
+function putHistory(data) {
+    if (checkForStorage()) {
+        let historyData = null;
+        if (localStorage.getItem(CACHE_KEY) === null) {
+            historyData = [];
+        } else {
+            historyData = Json.parse(localStorage.getItem(CACHE_KEY));
+        }
+
+        historyData.unshift(data);
+
+        if (historyData.length > 5) {
+            historyData.pop();
+        }
+
+        localStorage.setItem(CACHE_KEY, JSON.stringify(historyData));
+    }
 }
